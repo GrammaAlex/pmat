61c162b1ff72f132ce3d23a67633387014cfaeae
GrammaAlex <156843296+GrammaAlex@users.noreply.github.com>
2024-10-11 13:45:51 +0300

    Update 1(random).py

--- laba1/1(random).py
+++ laba1/1(random).py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-
 import random
+import sys
 
-A = random.randint(-10, 10)
-print (A)
+def main():
+    try:
+        A = random.randint(-10, 10)
+        print(A)
+    except Exception as e:
+        print(e, file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
